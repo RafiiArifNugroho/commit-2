bfe8f1347e21adc66ab2a33bebbe23087c5ee5f3
RafiiArifNugroho <rafialamcitra@gmail.com>
2025-05-05 11:39:08 +0700

    commit 15

--- FriendFunctionDua.cpp
+++ FriendFunctionDua.cpp
@@ -16,3 +16,11 @@ private:
 public:
     pelajar()(nilai = 100;) friend void manusia ::shownilaiPelajar(pelajar &x);
 };
+void manusia ::shownilaipelajar(pelajar &a)(
+    cout << x.nilai;)
+
+    int main()(
+        manusia budi;
+        pelajar pbudi;
+        budi.shownilaiPelajar(pBud);
+        return 0;)
