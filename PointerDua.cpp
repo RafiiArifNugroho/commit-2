fb29ebde725a8b90c88a71cfc09f315709a08aaf
RafiiArifNugroho <rafialamcitra@gmail.com>
2025-05-05 11:02:19 +0700

    commit tuju

--- PointerDua.cpp
+++ PointerDua.cpp
@@ -10,3 +10,13 @@ class mahasiswa
         cout << "No Induk = " << nim << endl;
     }
 };
+
+int main()
+{
+    mahasiswa *mhs = new mahasiswa; // Point to class object
+    mhs->nim = 1234567890;          // Accessing class member using pointer
+    mhs - showNim();                // Calling class method using pointer
+
+    delete mhs; // Freeing allocated memory
+    return 0;
+}
