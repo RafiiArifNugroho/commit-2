a229e4f4a726795589f58e855db619e00ad10415
RafiiArifNugroho <rafialamcitra@gmail.com>
2025-05-05 11:18:13 +0700

    commit sepuluh

--- FriendClassSatu.cpp
+++ FriendClassSatu.cpp
@@ -37,4 +37,11 @@ void siswa::setId(int pId)
 {
     id = pId;
 }
-int main
+int main()
+{
+    orang joko;
+    joko.setnama(" Joko susilo");
+    siswa joko_siswa;
+    joko_siswa.setId(1);
+    joko_siswa
+}
